83de1bc4428eeaaa18ac3897e7bcaa0ec8ea6c9a
David Erel <david@piiano.com>
2023-09-13 19:57:19 +0300

    support m2 folder as volume

--- cli/flows-cli.sh
+++ cli/flows-cli.sh
@@ -32,6 +32,33 @@ handle_error() {
     fi
 }
 
+# check validity of PIIANO_CS_M2_FOLDER
+# If it exists, it needs to have files in it
+# If it doesn't exist try ~/.m2 and fallback to CWD
+set_maven_folder() {
+  if [ ! -z "${PIIANO_CS_M2_FOLDER:-}" ] ; then
+    if [ ! -d "${PIIANO_CS_M2_FOLDER}/repository" ] ; then
+      echo "ERROR: ${PIIANO_CS_M2_FOLDER}/repository does not exist"
+      exit 1
+    fi
+    echo "[ ] Using ${PIIANO_CS_M2_FOLDER}/repository M2 folder"
+    return
+  else
+    echo "[ ] PIIANO_CS_M2_FOLDER is unset"
+  fi
+
+  if [ -d "${HOME}/.m2/repository" ] ; then
+    echo "[ ] Using ${HOME}/.m2 folder"
+    PIIANO_CS_M2_FOLDER=${HOME}/.m2
+  else
+    echo "[ ] Using M2 repository in `pwd`"
+    PIIANO_CS_M2_FOLDER=`pwd`/.m2
+    mkdir -p ${PIIANO_CS_M2_FOLDER}/repository
+  fi
+
+  export PIIANO_CS_M2_FOLDER
+}
+
 trap handle_error ERR
 
 # Verify prerequisites.
@@ -78,14 +105,18 @@ if ! is_absolute_path "${PATH_TO_SOURCE_CODE}" ; then
   exit 1
 fi
 
+
 # Create a volume for M2
 VOL_NAME=piiano_flows_m2_vol
-export PIIANO_CS_M2_FOLDER=${PIIANO_CS_M2_FOLDER:-"$HOME/.m2"}
-if docker volume inspect ${VOL_NAME} > /dev/null 2>&1; then
+
+if $(docker volume inspect ${VOL_NAME} > /dev/null 2>&1) ; then
   echo "[ ] Reusing volume ${VOL_NAME}. (to remove: docker volume rm ${VOL_NAME})"
 else
   echo -n "[ ] Creating volume for M2: "
   docker volume create ${VOL_NAME}
+
+  set_maven_folder
+  echo "[ ] Copying M2 folder ${PIIANO_CS_M2_FOLDER} to the volume"
   docker run --rm -v ${PIIANO_CS_M2_FOLDER}:/from -v ${VOL_NAME}:/to alpine sh -c "cp -r /from/* /to/"
 fi
 
