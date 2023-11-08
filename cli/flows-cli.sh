248887a3a84f51f213e8f1a2fa79d304cfc7f2b2
David Erel <david@piiano.com>
2023-11-08 17:05:40 +0200

    tweaking engine debug level

--- cli/flows-cli.sh
+++ cli/flows-cli.sh
@@ -11,6 +11,8 @@ PIIANO_CS_ENDPOINT_ROLE_TO_ASSUME=arn:aws:iam::211558624535:role/sagemaker-prod-
 PIIANO_CS_ENDPOINT_NAME=sagemaker-prod-endpoint
 PIIANO_CS_ENGINE_IMAGE=piiano/code-scanner:offline-${ENGINE_VERSION}
 PIIANO_CS_VIEWER_IMAGE=piiano/flows-viewer:${VIEWER_VERSION}
+PIIANO_CS_TAINT_ANALYZER_LOG_LEVEL="--verbosity=progress"
+
 PORT=${PORT:=3000}
 VOL_NAME=piiano_flows_m2_vol
 
