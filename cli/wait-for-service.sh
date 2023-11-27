a6ef1ca8459fc22535090a7ee610682d76c9b954
David Erel <david@piiano.com>
2023-11-27 09:26:33 +0200

    adding debugging

--- cli/wait-for-service.sh
+++ cli/wait-for-service.sh
@@ -13,7 +13,10 @@ PID="${3:-'0'}"
 
 SLEEP_BETWEEN_ATTEMPTS_SECS=3
 
+echo "Wait for service on ${service_url} for ${max_attempts} and PID=${PID}"
+
 # Poll the service until it's up or until the maximum number of attempts is reached
+set -x
 attempt_counter=0
 while [ "$attempt_counter" -lt "$max_attempts" ]; do
     response=$(curl --write-out '%{http_code}' --silent --output /dev/null "$service_url" || true)
@@ -22,16 +25,18 @@ while [ "$attempt_counter" -lt "$max_attempts" ]; do
         echo "Service is up and returned HTTP 200"
         exit 0
     else
-        echo "Service returned HTTP $response. Retrying ${attempt_counter}..."
         ((attempt_counter++))
         if [ "$attempt_counter" -eq "$max_attempts" ]; then
             echo "Max attempts reached. Exiting."
             exit 1
       fi
-      if [ "${PID}" != "0" ] && ! ps -p ${PID} > /dev/null; then
-          echo "Process ${PID} has died, exit."
-          exit 1
+      if [ "${PID}" != "0" ] ; then
+          if ! ps -p ${PID} > /dev/null 2>&1 ; then
+            echo "Process ${PID} has died, exit."
+            exit 1
+          fi
       fi
+      echo "Service returned HTTP $response. Retrying ${attempt_counter}..."
       sleep ${SLEEP_BETWEEN_ATTEMPTS_SECS}
     fi
 done
