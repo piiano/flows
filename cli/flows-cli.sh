7ed99832f290364287bbac181a7b0e271f8e5f1b
nerya10 <91833983+nerya10@users.noreply.github.com>
2024-06-16 18:57:52 +0300

    Fix basename

--- cli/flows-cli.sh
+++ cli/flows-cli.sh
@@ -445,7 +445,7 @@ else
       -e "PIIANO_CS_SCAN_ID_EXTERNAL=${PIIANO_CS_SCAN_ID_EXTERNAL:-}" \
       --env-file <(env | grep PIIANO_CS) \
       -v "${PATH_TO_SOURCE_CODE}:/source" ${VOLUME_DOCKER_FLAGS[@]:-} \
-      -v "${DIAG_FILE_PATH}:/stats/(basename $DIAG_FILE_PATH)" \
+      -v "${DIAG_FILE_PATH}:/stats/$(basename $DIAG_FILE_PATH)" \
       --ulimit nofile=${MAX_NUM_OF_FILES_CONTAINER}:${MAX_NUM_OF_FILES_CONTAINER} \
       ${PIIANO_CS_ENGINE_IMAGE} ${EXTRA_TEST_PARAMS[@]:-}
 fi
